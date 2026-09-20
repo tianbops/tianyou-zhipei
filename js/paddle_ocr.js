b40b7a03e3aaebb48d5ff49971108cc5acf973c0
tianbops <tianbops@gmail.com>
2026-09-20 08:37:38 +0800

    fix: 修复OCR脚本截断导致图片选择后未进入识别

--- js/paddle_ocr.js
+++ js/paddle_ocr.js
@@ -298,3 +298,12 @@
   bindUploadInput('ocrAlbumInput', 'album');
   bindUploadInput('ocrFileInput', 'file');
 
+
+      });
+    });
+  }
+
+  bindUploadInput('ocrCameraInput', 'camera');
+  bindUploadInput('ocrAlbumInput', 'album');
+  bindUploadInput('ocrFileInput', 'file');
+})();
