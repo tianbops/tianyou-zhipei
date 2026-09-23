dba9fee59d4104555c82860005d5b6c28cc1f736
tianbops <tianbops@gmail.com>
2026-09-23 10:01:01 +0800

    fix: centralize miniprogram logout cleanup

--- miniprogram/pages/settings/settings.js
+++ miniprogram/pages/settings/settings.js
@@ -1,7 +1,8 @@
 const app = getApp();
+const { clearSession } = require('../../utils/session');
 Page({
  data:{user:{}},
  onShow(){this.setData({user:app.globalData.user||wx.getStorageSync('zhipei_user')||{}})},
  changePassword(){wx.showToast({title:'请在网页端修改密码',icon:'none'})},
- logout(){wx.removeStorageSync('zhipei_token');wx.removeStorageSync('zhipei_user');app.globalData.token='';app.globalData.user=null;wx.reLaunch({url:'/pages/login/login'})}
+ logout(){clearSession();wx.reLaunch({url:'/pages/login/login'})}
 });
